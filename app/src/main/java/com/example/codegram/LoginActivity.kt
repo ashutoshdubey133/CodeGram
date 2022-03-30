f3917f00bd3e645a86e775f951b69e82af9e4eb6
ashutoshdubey133 <ashutoshdubey133@gmail.com>
2022-03-31 00:28:05 +0530

    Added View Binding.

--- app/src/main/java/com/example/codegram/LoginActivity.kt
+++ app/src/main/java/com/example/codegram/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.example.codegram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.codegram.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+//        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
